0db1961564c0f0972593f90fd360d2c35c8d62ac
qbasty <58854397+qbasty@users.noreply.github.com>
2023-04-07 22:01:42 +0200

    0.0.1.1

--- src/main/java/net/blockhost/livechattranslate/LiveChatTranslate.java
+++ src/main/java/net/blockhost/livechattranslate/LiveChatTranslate.java
@@ -3,7 +3,6 @@ package net.blockhost.livechattranslate;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LiveChatTranslate extends JavaPlugin {
-
     @Override
     public void onEnable() {
         String apiKey = getConfig().getString("deepl-api-key");
@@ -11,5 +10,4 @@ public final class LiveChatTranslate extends JavaPlugin {
         TranslationService translationService = new TranslationService(apiKey);
         getServer().getPluginManager().registerEvents(new TranslateChatListener(this, translationService), this);
     }
-
 }
--- src/main/java/net/blockhost/livechattranslate/TranslateChatListener.java
+++ src/main/java/net/blockhost/livechattranslate/TranslateChatListener.java
@@ -22,13 +22,18 @@ public class TranslateChatListener implements Listener {
         Player sender = event.getPlayer();
         String message = event.getMessage();
 
+        plugin.getLogger().info("Received chat message: " + message);
+
         String language = translationService.detectLanguage(message);
+        plugin.getLogger().info("Detected language: " + language);
 
         if ("en".equals(language)) {
             for (Player recipient : event.getRecipients()) {
                 String locale = recipient.getLocale();
+                plugin.getLogger().info("Recipient " + recipient.getName() + " has locale: " + locale);
                 if (locale.startsWith("es_")) {
                     String translatedMessage = translationService.translate(message, "ES");
+                    plugin.getLogger().info("Translated message: " + translatedMessage);
                     recipient.sendMessage(String.format(event.getFormat(), sender.getDisplayName(), translatedMessage));
                 } else {
                     recipient.sendMessage(String.format(event.getFormat(), sender.getDisplayName(), message));
